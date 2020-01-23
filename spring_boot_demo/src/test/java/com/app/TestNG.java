2ebf7533c5292f3f02a224598e7d20e1e77c787e
Sonali Kanse <sonali123@cdac.in>
2020-01-23 13:56:13 +0530

    spring_boot_demo modified

--- spring_boot_demo/src/test/java/com/app/TestNG.java
+++ spring_boot_demo/src/test/java/com/app/TestNG.java
@@ -26,11 +26,7 @@ public class TestNG {
 		String projectPath = System.getProperty("user.dir");
 		PropertiesFile.getProperties();
 		
-		if(browserName.equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/chromedriver/chromedriver.exe");
-			driver = new ChromeDriver();
-		}
-		else if(browserName.equalsIgnoreCase("firefox")) {
+		 if(browserName.equalsIgnoreCase("firefox")) {
 			System.setProperty("webdriver.gecko.driver", projectPath+"/drivers/geckodriver/geckodriver");
 			driver = new FirefoxDriver();
 		}
