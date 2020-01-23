3ae508a605a37027bc3fdd0a22caa228d6426dcc
Sonali Kanse <sonali123@cdac.in>
2020-01-23 12:12:51 +0530

    spring boot demo project modified

---
+++ spring_boot_demo/src/main/java/com/app/log4j/Log4jDemo.java
@@ -0,0 +1,26 @@
+package com.app.log4j;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4jDemo {
+
+	
+	private static Logger logger = LogManager.getLogger(Log4jDemo.class);
+	
+	public static void main(String[] args) {
+		
+		System.out.println("\n  Hello World...!   \n");
+		
+		logger.trace("This is trace messages");
+		logger.info("This is information message");
+		logger.error("This is an error message");
+		logger.warn("This is a warning message");
+		logger.fatal("This is a fatal message");
+
+		System.out.println("\n  Completed");
+		
+		
+		
+	}
+}
---
+++ spring_boot_demo/src/test/java/com/app/TestNG.java
@@ -0,0 +1,73 @@
+package com.app;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.app.config.PropertiesFile;
+import com.app.log4j.Log4jDemo;
+
+public class TestNG {
+	
+	WebDriver driver = null;
+	public static String browserName=null;
+	private static Logger logger = LogManager.getLogger(Log4jDemo.class);
+	
+	
+	@BeforeTest
+	public void setUpTest() {
+		String projectPath = System.getProperty("user.dir");
+		PropertiesFile.getProperties();
+		
+		if(browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/chromedriver/chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+		else if(browserName.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver", projectPath+"/drivers/geckodriver/geckodriver");
+			driver = new FirefoxDriver();
+		}
+		
+		
+		logger.info("Browser started");
+		
+	}
+
+	@Test
+	public void googleSearch() {
+			
+		//goto google.com
+		driver.get("http://localhost:4200/search");
+		logger.info("Navigated to webapplication UI");
+		
+		//enter text in search textbox
+		//driver.findElement(By.name("q")).sendKeys("Automation Step by Step");
+		
+		//click on search button
+		//driver.findElement(By.name("btnK")).click();
+		//driver.findElement(By.id("1"));
+
+		//driver.findElement(By.name("btnK")).sendKeys(Keys.RETURN);
+		
+	}
+	
+	@AfterTest
+	public void tearDownTest() {
+		
+		//close browser
+				driver.close();
+				//driver.quit();
+				System.out.println("Test Completed Successfully");
+				PropertiesFile.setProperties();
+		
+	}
+
+
+}
