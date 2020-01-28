bf8c2184b4325b994151d1122c83fa6ddcd4ece9
Sonali Kanse <sonali123@cdac.in>
2020-01-28 17:04:59 +0530

    changed file

--- spring_boot_demo/src/test/java/com/app/TestNG.java
+++ spring_boot_demo/src/test/java/com/app/TestNG.java
@@ -35,7 +35,7 @@ public class TestNG {
 			ChromeOptions options = new ChromeOptions();
 			//options.setExperimentalOption("prefs", chromePrefs);
 			options.addArguments("--no-sandbox");
-			//options.addArguments("--headless"); //should be enabled for Jenkins
+			options.addArguments("--headless"); //should be enabled for Jenkins
 			//options.addArguments("--disable-dev-shm-usage"); //should be enabled for Jenkins
 			//options.addArguments("--window-size=1920x1080"); //should be enabled for Jenkins
 			driver = new ChromeDriver(options);
