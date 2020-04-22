4d2bb1b6257efeae8ab73fcdef19da256c140fe1
CDAC_ECGC <kansesonali1995@gmail.com>
2020-04-22 14:00:55 +0530

    Delted all dump.xml file and modified all three microservices application.properties

--- hrd_emp_be/src/main/java/in/ecgc/erp/hrd_emp_be/model/Employee.java
+++ hrd_emp_be/src/main/java/in/ecgc/erp/hrd_emp_be/model/Employee.java
@@ -12,7 +12,7 @@ import lombok.Data;
  *@Author Architecture Team C-DAC Mumbai
  *
  **/
-@Data
+//@Data
 public class Employee {
 	int empId;
 	
@@ -29,4 +29,69 @@ public class Employee {
 	String empType;
 	
 	String designation;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMidName() {
+		return midName;
+	}
+
+	public void setMidName(String midName) {
+		this.midName = midName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getDoj() {
+		return doj;
+	}
+
+	public void setDoj(String doj) {
+		this.doj = doj;
+	}
+
+	public String getEmpType() {
+		return empType;
+	}
+
+	public void setEmpType(String empType) {
+		this.empType = empType;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	
 }
