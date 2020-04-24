17e48c8fff13ab7888fc3ce91a565eac28e510cd
CDAC_ECGC <kansesonali1995@gmail.com>
2020-04-24 14:37:09 +0530

    Added TestNG Controller

--- hrd_emp_be/src/test/java/in/ecgc/erp/hrd_emp_be/TestNGController.java
+++ hrd_emp_be/src/test/java/in/ecgc/erp/hrd_emp_be/TestNGController.java
@@ -3,27 +3,30 @@ package in.ecgc.erp.hrd_emp_be;
 import static in.ecgc.erp.hrd_emp_be.JsonConverter.asJsonString;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Before;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import in.ecgc.erp.hrd_emp_be.model.Employee;
 
-public class TestController extends HrdEmpBeApplicationTests {
+@SpringBootTest(classes = HrdEmpBeApplication.class)
+public class TestNGController extends AbstractTestNGSpringContextTests {
+	
 	@Autowired
 	private WebApplicationContext webApplicationContext;
 
 	//@Autowired
 	private MockMvc mockMvc;
 
-	@Before
+	@BeforeClass
 	public void setup() {
 		
 		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
